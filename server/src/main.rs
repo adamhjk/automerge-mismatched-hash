26b5e1d9ba3115c6fdba50bc137631bd40324834
Adam Jacob <adam@systeminit.com>
2021-03-03 13:24:34 -0800

    only happens with numbers

--- server/src/main.rs
+++ server/src/main.rs
@@ -5,73 +5,7 @@ use warp::Filter;
 async fn main() -> Result<()> {
     let good_json = serde_json::json!({ "bobo": { "job": "clown", "friendly": true } });
 
-    let bad_json = serde_json::json!(
-    {
-      "id": "entity:56021541879545914",
-      "name": "descriptive-meeting-0524",
-      "objectType": "kubernetesDeployment",
-      "description": "descriptive-meeting-0524",
-      "expressionProperties": { "__baseline": {} },
-      "manualProperties": { "__baseline": {} },
-      "inferredProperties": {
-        "__baseline": {
-          "kubernetesObject": {
-            "kind": "Deployment",
-            "spec": {
-              "replicas": 1,
-              "selector": { "matchLabels": { "app": "descriptive-meeting-0524" } },
-              "template": {
-                "metadata": { "labels": { "app": "descriptive-meeting-0524" } }
-              }
-            },
-            "metadata": { "name": "descriptive-meeting-0524-deployment" },
-            "apiVersion": "apps/v1"
-          }
-        }
-      },
-      "properties": {
-        "__baseline": {
-          "kubernetesObject": {
-            "kind": "Deployment",
-            "spec": {
-              "replicas": 1,
-              "selector": { "matchLabels": { "app": "descriptive-meeting-0524" } },
-              "template": {
-                "metadata": { "labels": { "app": "descriptive-meeting-0524" } }
-              }
-            },
-            "metadata": { "name": "descriptive-meeting-0524-deployment" },
-            "apiVersion": "apps/v1"
-          },
-          "kubernetesObjectYaml": "apiVersion: apps/v1\nkind: Deployment\nmetadata:\n  name: descriptive-meeting-0524-deployment\nspec:\n  selector:\n    matchLabels:\n      app: descriptive-meeting-0524\n  replicas: 1\n  template:\n    metadata:\n      labels:\n        app: descriptive-meeting-0524\n"
-        }
-      },
-      "nodeId": "node:56021541829214265",
-      "head": false,
-      "base": false,
-      "siStorable": {
-        "typeName": "entity",
-        "objectId": "entity:56021541879545914",
-        "billingAccountId": "billingAccount:56014734046003223",
-        "organizationId": "organization:56014739397935131",
-        "workspaceId": "workspace:56014739406323740",
-        "tenantIds": [
-          "billingAccount:56014734046003223",
-          "organization:56014739397935131",
-          "workspace:56014739406323740",
-          "entity:56021541879545914"
-        ],
-        "createdByUserId": null,
-        "updateClock": { "epoch": 0, "updateCount": 50 },
-        "deleted": false
-      },
-      "siChangeSet": {
-        "changeSetId": "changeSet:56014879538020398",
-        "editSessionId": "editSession:56014879571574831",
-        "event": "create",
-        "orderClock": { "epoch": 0, "updateCount": 5 }
-      }
-    });
+    let bad_json = serde_json::json!({ "number": 1 });
 
     let good_doc = {
         let initial_state = automerge::Value::from_json(&good_json);
